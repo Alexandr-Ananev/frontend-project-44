579b3d688bdeeb6e0838b2ac0875968337980da6
Alexandr Ananev <sanyches1978@gmail.com>
2025-10-25 14:33:16 +0200

    fix eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,27 +1,10 @@
 import js from '@eslint/js'
 import globals from 'globals'
-import stylistic from '@stylistic/eslint-plugin'
 import { defineConfig } from 'eslint/config'
+import stylistic from '@stylistic/eslint-plugin'
 
 export default defineConfig([
-  {
-    files: ['**/*.{js,mjs,cjs}', 'bin/*'],
-    plugins: {
-      js,
-      '@stylistic': stylistic,
-    },
-    extends: ['js/recommended'],
-    languageOptions: { globals: globals.browser },
-    rules: {
-      'quotes': ['error', 'single'], // одинарные кавычки
-      'indent': ['error', 2], // отступ 2 пробела
-      'no-multi-spaces': ['error'], // запрещает несколько пробелов
-      'no-console': 'off', // разрешить console.log
-      '@stylistic/semi': ['error', 'never'], // запрещает лишние точки с запятой
-      '@stylistic/no-trailing-spaces': 'error', // запрещает пробелы в конце строки
-      '@stylistic/eol-last': ['error', 'always'], // требует перевод строки в конце файла
-      '@stylistic/no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0 }], // максимум 1 пустая строка внутри кода и 0 в конце кода
-      '@stylistic/spaced-comment': ['error', 'always'], // пробел после // и внутри /* */
-    },
-  },
+  stylistic.configs.recommended,
+  { files: ['**/*.{js,mjs,cjs}'], plugins: { js }, extends: ['js/recommended'] },
+  { files: ['**/*.{js,mjs,cjs}'], languageOptions: { globals: globals.node } },
 ])
